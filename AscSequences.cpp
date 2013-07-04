3096b747eb850190d0eccae4c059261585e364d6
d-sherstennikov <d-shr@yandex.ru>
2013-07-04 02:49:58 -0700

    Update AscSequences.cpp

--- AscSequences.cpp
+++ AscSequences.cpp
@@ -1,4 +1,18 @@
 // Being given numbers N and K, output all ASC sequences of length K from numbers 1..N lexigraphically ordered.
+// input: 5 3
+// output: 
+/*
+1 2 3
+1 2 4
+1 2 5
+1 3 4
+1 3 5
+1 4 5
+2 3 4
+2 3 5
+2 4 5
+3 4 5
+*/
 
 #include <cstdio>
 #include <cstdlib>
